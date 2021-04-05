4e549cb6451d99e499d7e012eb6303adb03c4269
Osetinskiy-pokemon <71440118+Osetinskiy-pokemon@users.noreply.github.com>
2021-04-05 13:12:24 +0300

    Add files via upload

---
+++ TrelloApp_v1/Khamikoeva_PI19-3/src/main/java/com/example/seminar_project/KhamikoevaPi193Application.java
@@ -0,0 +1,13 @@
+package com.example.seminar_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhamikoevaPi193Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhamikoevaPi193Application.class, args);
+    }
+
+}
---
+++ TrelloApp_v1/Khamikoeva_PI19-3/src/main/java/com/example/seminar_project/entity/StatusTask.java
@@ -0,0 +1,28 @@
+package com.example.seminar_project.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "S_STATUS_WORK")
+@NoArgsConstructor
+public class StatusTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long rowId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "status_id", nullable = false)
+    private Status status;
+
+    @OneToMany(mappedBy = "statusTask")
+    private Set<Task> task;
+}
+
